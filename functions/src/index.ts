e8ed5da4dbd328f5c56801e9eda69decf5a071d3
Sam Echikson <samuel.echikson@gmail.com>
2019-09-02 15:57:28 -0400

    Add subscriptions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,14 @@
 import * as functions from 'firebase-functions';
 
-export const testFunction = functions.https.onCall( async (data, context) => {
-    const uid  = context.auth && context.auth.uid;
+export const testFunction = functions.https.onCall(async (data, context) => {
+    const uid = context.auth && context.auth.uid;
     const message = data.message;
 
     return `${uid} sent a message of ${message}`
 });
 
 
-export { stripeAttachSource } from './sources';
+export {stripeAttachSource} from './sources';
 
-export { stripeCreateCharge, stripeGetCharges } from './charges';
+export {stripeCreateCharge, stripeGetCharges} from './charges';
+export {stripeGetSubscriptions, stripeCancelSubscription, stripeCreateSubscription} from './subscriptions';
