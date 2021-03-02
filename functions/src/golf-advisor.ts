5411e968009eac6da5f8cac54645c1c3d457d484
samechikson <sechikson@takescoop.com>
2021-03-02 14:21:50 -0800

    Fix host for golf advisor

--- functions/src/golf-advisor.ts
+++ functions/src/golf-advisor.ts
@@ -1,12 +1,17 @@
 const functions = require("firebase-functions");
 const helpers = require("./helpers");
 const request = require("request");
+
+const hostname = 'www.golfpass.com'
+
 const getGolfDataById = (golfAdvisorCourseId) => {
     const options = {
         method: 'GET',
-        url: 'https://www.golfadvisor.com/ajax/course-layout/',
+        url: `https://${hostname}/ajax/course-layout/`,
         qs: { id: golfAdvisorCourseId },
     };
+    console.log("🚀 ~ file: golf-advisor.ts ~ line 13 ~ getGolfDataById ~ options", options)
+    
     return new Promise((resolve, reject) => {
         request(options, function (error, response, body) {
             if (error)
